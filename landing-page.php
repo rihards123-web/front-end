e323a295c1eae7b84fa5f99c457de3710b8ab93d
rihards123-web <rihardsnol@gmail.com>
2025-02-13 19:38:56 +0200

    working on landing page

--- landing-page.php
+++ landing-page.php
@@ -18,5 +18,23 @@ include('nav-bar.php');
             <button>Discover our collection</button>
         </div>
     </section>
+<!-- 
+    <section class="products">
+        te vajadzēs izvilkt kaut kādus produktus no db, lai uztaisītu ķkipa showcase ar dažiem no tiem
+    </section> -->
+    
+    <section class="deals">
+        <div class="deals-left">
+            <h2>Exclusive deals<br>and offers</h2>
+            <h4>Made for your covenience</h4>
+            <p><img src="media/checkmark-circle-outline 1.png" alt=""><strong>Eco-sustainable:</strong> All recyclable materials, 0% CO2 emissions</p>
+            <p><img src="media/checkmark-circle-outline 1.png" alt=""><strong>Hypoallergenic:</strong> 100% natural, human friendly ingredients </p>
+            <p><img src="media/checkmark-circle-outline 1.png" alt=""><strong>Trendy:</strong> accesorries and clothing items as seen in latest trends</p>
+            <button>Learn More</button>
+        </div>
+        <div class="deals-right">
+            <img src="media/stock.png" alt="">
+        </div>
+    </section>
 </body>
 </html>
